699a09ccc72fae3b0793a397b75840727d742b18
yaman Dudhat <dyaman@alita.local>
2023-09-26 11:29:50 +0530

    Minor changes in functionality

--- functions/Controller.js
+++ functions/Controller.js
@@ -8,13 +8,21 @@ const path = require('path');
 var CryptoJS = require("crypto-js");
 require("dotenv").config()
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
-const {STATUS_CODE, STATUS_ERROR} = require("./commonConstant");
+const {
+    STATUS_CODE,
+    STATUS_ERROR,
+    // COMMENT,
+    // NEW_TICKET,
+    SUPPORT_REQUEST,
+    REFRESH_TOKEN
+} = require("./commonConstant");
 const {
     decryptAccessToken,
     getZohoHeaders,
     refreshAccessToken,
-    CommonFunForCatch,
-    removeTags
+    commonFunForCatch,
+    truncateToDecimals,
+    sanitizeHtml
 } = require('./services/commonFuncions');
 
 
@@ -24,9 +32,10 @@ exports.RefreshAccessToken = async (req, res) => {
     const clientSecret = process.env.ZOHO_CLIENT_SECRET;
     const refreshToken = process.env.ZOHO_REFRESH_TOKEN;
     const refreshTokenURL = `${process.env.REFRESH_TOKEN_URL}/token`;
+    const grantType = REFRESH_TOKEN
 
     const data = {
-        grant_type: 'refresh_token',
+        grant_type: grantType,
         client_id: clientId,
         client_secret: clientSecret,
         refresh_token: refreshToken,
@@ -78,8 +87,7 @@ exports.addUser = async (req, res) => {
     const zohoApiBaseUrl = `${process.env.ZOHO_CRM_V2_URL}/Customer`;
 
     try {
-        const accessToken = req.headers.authorization.split(" ")[1];
-        const decryptToken = decryptAccessToken(accessToken, process.env.SECRET_KEY);
+        const decryptToken = decryptAccessToken(req, process.env.SECRET_KEY);
 
         const checkUserResponse = await axios.get(`${zohoApiBaseUrl}/search?criteria=(Email:equals:${req.body.data[0].Email})`, {
             headers: getZohoHeaders(decryptToken)
@@ -87,7 +95,7 @@ exports.addUser = async (req, res) => {
 
         if (checkUserResponse.status !== 200) {
 
-            const response = await axios.post(zohoApiBaseUrl, removeTags(JSON.stringify(req.body)), {
+            const response = await axios.post(zohoApiBaseUrl, sanitizeHtml(JSON.stringify(req.body)), {
                 headers: getZohoHeaders(decryptToken)
             });
 
@@ -124,7 +132,7 @@ exports.addUser = async (req, res) => {
                     headers: getZohoHeaders(decryptToken)
                 });
                 if (checkUserResponse.status !== 200) {
-                    const responseData = await CommonFunForCatch(zohoApiBaseUrl, 'post', `${decryptToken}`, removeTags(JSON.stringify(req.body)));
+                    const responseData = await commonFunForCatch(zohoApiBaseUrl, 'post', `${decryptToken}`, sanitizeHtml(JSON.stringify(req.body)));
                     return res.status(200).send(responseData);
                 } else {
                     return res.status(checkUserResponse.status).json(checkUserResponse.data);
@@ -144,13 +152,11 @@ exports.Payment = async (req, res) => {
     const zohoApiBaseUrlforPayment = `${process.env.ZOHO_CRM_V2_URL}/Payment`;
 
     try {
-        const accessToken = req.headers.authorization.split(" ")[1];
-        const decryptToken = decryptAccessToken(accessToken, process.env.SECRET_KEY);
+        const decryptToken = decryptAccessToken(req, process.env.SECRET_KEY);
 
-        const response = await axios.post(zohoApiBaseUrlforPayment, removeTags(JSON.stringify(req.body)), {
+        const response = await axios.post(zohoApiBaseUrlforPayment, sanitizeHtml(JSON.stringify(req.body)), {
             headers: getZohoHeaders(decryptToken)
         });
-
         if (response.status === 200 || response.status === 201) {
             const responseData = response.data;
             if (responseData && responseData.data && responseData.data[0].details.id) {
@@ -179,7 +185,7 @@ exports.Payment = async (req, res) => {
             const decryptToken = decryptAccessToken(newAccessToken, process.env.SECRET_KEY);
 
             try {
-                const responseData = await CommonFunForCatch(zohoApiBaseUrlforPayment, 'post', `${decryptToken}`, removeTags(JSON.stringify(req.body)));
+                const responseData = await commonFunForCatch(zohoApiBaseUrlforPayment, 'post', `${decryptToken}`, sanitizeHtml(JSON.stringify(req.body)));
                 return res.status(200).send(responseData);
             } catch (error) {
                 return res.status(500).json({ message: req.t("CATCH_ERROR") });
@@ -195,10 +201,9 @@ exports.Order = async (req, res) => {
     const zohoApiBaseUrlforOrder = `${process.env.ZOHO_CRM_V5_URL}/Sales_Orders`;
 
     try {
-        const accessToken = req.headers.authorization.split(" ")[1];
-        const decryptToken = decryptAccessToken(accessToken, process.env.SECRET_KEY);
+        const decryptToken = decryptAccessToken(req, process.env.SECRET_KEY);
 
-        const response = await axios.post(zohoApiBaseUrlforOrder, removeTags(JSON.stringify(req.body)), {
+        const response = await axios.post(zohoApiBaseUrlforOrder, sanitizeHtml(JSON.stringify(req.body)), {
             headers: getZohoHeaders(decryptToken)
         });
         if (response.status === 200 || response.status === 201) {
@@ -229,7 +234,7 @@ exports.Order = async (req, res) => {
             const decryptToken = decryptAccessToken(newAccessToken, process.env.SECRET_KEY);
 
             try {
-                const responseData = await CommonFunForCatch(zohoApiBaseUrlforOrder, 'post', `${decryptToken}`, removeTags(JSON.stringify(req.body)));
+                const responseData = await commonFunForCatch(zohoApiBaseUrlforOrder, 'post', `${decryptToken}`, sanitizeHtml(JSON.stringify(req.body)));
                 return res.status(200).send(responseData);
             } catch (error) {
                 return res.status(500).json({ message: req.t("CATCH_ERROR") });
@@ -245,10 +250,9 @@ exports.Invoice = async (req, res) => {
     const zohoApiBaseUrlforInvoice = `${process.env.ZOHO_CRM_V5_URL}/Invoices`;
 
     try {
-        const accessToken = req.headers.authorization.split(" ")[1];
-        const decryptToken = decryptAccessToken(accessToken, process.env.SECRET_KEY);
+        const decryptToken = decryptAccessToken(req, process.env.SECRET_KEY);
 
-        const response = await axios.post(zohoApiBaseUrlforInvoice, removeTags(JSON.stringify(req.body)), {
+        const response = await axios.post(zohoApiBaseUrlforInvoice, sanitizeHtml(JSON.stringify(req.body)), {
             headers: getZohoHeaders(decryptToken)
         });
         if (response.status === 200 || response.status === 201) {
@@ -265,7 +269,7 @@ exports.Invoice = async (req, res) => {
                     const invoiceData = userResponseData.data[0];
                     const mailgun = new Mailgun(formData);
                     const client = mailgun.client({
-                        username: 'api',
+                        username: process.env.MAILGUN_USERNAME,
                         key: process.env.API_KEY,
                         url: process.env.MAILGUN_URL
                     });
@@ -348,7 +352,7 @@ exports.Invoice = async (req, res) => {
             const decryptToken = decryptAccessToken(newAccessToken, process.env.SECRET_KEY);
 
             try {
-                const responseData = await CommonFunForCatch(zohoApiBaseUrlforInvoice, 'post', `${decryptToken}`, removeTags(JSON.stringify(req.body)));
+                const responseData = await commonFunForCatch(zohoApiBaseUrlforInvoice, 'post', `${decryptToken}`, sanitizeHtml(JSON.stringify(req.body)));
                 return res.status(200).send(responseData);
             } catch (error) {
                 return res.status(500).json({ message: req.t("CATCH_ERROR") });
@@ -365,17 +369,16 @@ exports.Support = async (req, res) => {
     const zohoApiBaseUrl = `${process.env.ZOHO_CRM_V2_URL}/Customer`;
 
     try {
-        const accessToken = req.headers.authorization.split(" ")[1];
-        const decryptToken = decryptAccessToken(accessToken, process.env.SECRET_KEY);
+        const decryptToken = decryptAccessToken(req, process.env.SECRET_KEY);
 
         const checkUserResponse = await axios.get(`${zohoApiBaseUrl}/search?criteria=(Email:equals:${req.body.data[0].Email})`, {
             headers: getZohoHeaders(decryptToken)
         });
-        const requestType = checkUserResponse.status === 200 ? "Comment" : "New Ticket";
+        const requestType = checkUserResponse.status === 200 ? SUPPORT_REQUEST.COMMENT : SUPPORT_REQUEST.NEW_TICKET;
 
         req.body.data[0].Request_Type = [requestType];
 
-        const response = await axios.post(zohoApiBaseUrlForSupport, removeTags(JSON.stringify(req.body)), {
+        const response = await axios.post(zohoApiBaseUrlForSupport, sanitizeHtml(JSON.stringify(req.body)), {
             headers: getZohoHeaders(decryptToken)
         });
         if (response.status === 200 || response.status === 201) {
@@ -397,12 +400,12 @@ exports.Support = async (req, res) => {
             const checkUserResponse = await axios.get(`${zohoApiBaseUrl}/search?criteria=(Email:equals:${req.body.data[0].Email})`, {
                 headers: getZohoHeaders(decryptToken)
             });
-            const requestType = checkUserResponse.status === 200 ? "Comment" : "New Ticket";
+            const requestType = checkUserResponse.status === 200 ? SUPPORT_REQUEST.COMMENT : SUPPORT_REQUEST.NEW_TICKET;
 
             req.body.data[0].Request_Type = [requestType];
 
             try {
-                const responseData = await CommonFunForCatch(zohoApiBaseUrlForSupport, 'post', `${decryptToken}`, removeTags(JSON.stringify(req.body)));
+                const responseData = await commonFunForCatch(zohoApiBaseUrlForSupport, 'post', `${decryptToken}`, sanitizeHtml(JSON.stringify(req.body)));
                 return res.status(200).json({ data: responseData, message: req.t("SUPPORT_MESSAGE") });
             } catch (error) {
                 return res.status(500).json({ message: req.t("CATCH_ERROR") });
@@ -412,9 +415,3 @@ exports.Support = async (req, res) => {
         }
     }
 };
-
-// this function is to truncate the additional decimal points
-function truncateToDecimals(num, dec = 2) {
-    const calcDec = Math.pow(10, dec);
-    return Math.trunc(num * calcDec) / calcDec;
-}
